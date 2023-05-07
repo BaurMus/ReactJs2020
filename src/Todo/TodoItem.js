0a2c6be82a9104ec0faefaf284e04ad113b574de
BaurMus <musilimov_b@mail.ru>
2023-05-07 11:56:18 +0600

    create context and append Todo input form

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
+import Context from '../context';
 
 const styles = {
   li: {
@@ -16,28 +17,38 @@ const styles = {
   }
 };
 
-function TodoItem({todo, index}) {
+function TodoItem({todo, index, onChange}) {
+  const {removeTodo} = useContext(Context);
+
+  const classes = [];
+
+  if (todo.completed) {
+    classes.push('done');
+  }
+
   return (
     <li style={styles.li}>
-      <span>
+      <span className={classes.join(' ')}>
         <input 
           type="checkbox" 
+          checked={todo.completed}
           style={styles.input}
-          onChange={() => console.log(todo.id)}  
+          onChange={() => onChange(todo.id)}  
         />
         <strong>{index + 1}</strong>
         &nbsp;
         {todo.title}
       </span>
 
-      <button className='rm'>&times;</button>
+      <button className='rm' onClick={removeTodo.bind(null, todo.id)}>&times;</button>
     </li>
   )
 }
 
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
-  index: PropTypes.number
+  index: PropTypes.number,
+  onChange: PropTypes.func.isRequired
 };
 
 export default TodoItem
