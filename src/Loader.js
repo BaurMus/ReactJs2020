d005b720eeeae701b665b8ec6a7f10c718c122f1
BaurMus <musilimov_b@mail.ru>
2023-05-07 15:24:26 +0600

    install React.suspence(React.lazy) for input form

--- src/Loader.js
+++ src/Loader.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default () => <div style={{display: 'flex', justifyContent: 'center', margin: '.5rem'}}>
+export default () => (<div style={{display: 'flex', justifyContent: 'center', margin: '.5rem'}}>
   <div className="lds-dual-ring"></div>
-</div>
+</div>);
